ad5f5c937942137ae8ff14fbd91de30ef69dba68
Michel Gokan <gokan.michel@gmail.com>
2018-08-06 11:07:36 +0200

    Add description

--- app.js
+++ app.js
@@ -8,5 +8,6 @@ app.get('/mem', (req, res) => res.send(workloads.memoryIntensiveWorkload().toStr
 app.get('/disk', (req, res) => res.send(workloads.diskIntensiveWorkload().toString()));
 app.get('/net', (req, res) => res.send(workloads.networkIntensiveWorkload().toString()));
 app.get('/x', (req, res) => res.send(workloads.combinedWorkload().toString()));
+app.get('/', (req, res) => res.send("Hi :)! Please use one of the following endpoints:<br />* /cpu for CPU intensive workloads<br />* /mem for memory intensive workloads<br />* /disk for disk intensive workloads<br />* /net for network intensive workloads<br />* /x for combined workloads");
 
 app.listen(30005, "0.0.0.0");
