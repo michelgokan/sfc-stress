271d5d16464a6ff954a132d2d57c98c6e95313f6
Michel Gokan <michel.gokan@kau.se>
2020-10-07 10:35:08 +0200

    testing stress-ng...

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM node:lts
 WORKDIR /usr/src/app
 COPY package*.json ./
 RUN npm install
+RUN apt-get update
+RUN apt-get install stress-ng -y
 COPY . .
 EXPOSE 30005
 CMD [ "npm", "start" ]
--- workloads/mem-workload.js
+++ workloads/mem-workload.js
@@ -1,15 +1,18 @@
 const {Worker, isMainThread, parentPort, workerData} = require('worker_threads');
-let loadash = require('lodash');
+// let loadash = require('lodash');
+const execSync = require('child_process').execSync;
 
 async function executeMemWorkload(dataSize) {
-    const onemb = require('./payload/onemb');
+    // const onemb = require('./payload/onemb');
 
-    let bigStrArray = [];
-    for (let i = 0; i < dataSize; i++) {
-        bigStrArray.push(loadash.cloneDeep(onemb));
-    }
+    // let bigStrArray = [];
+    // for (let i = 0; i < dataSize; i++) {
+    return execSync('stress-ng --vm-bytes 4294967296 --vm 1 --vm-ops 100000', {stdio: 'pipe'});
+    // return cmd.get('stress-ng --vm-bytes 4294967296 --vm 1 --vm-ops 100000');
+    // bigStrArray.push(loadash.cloneDeep(onemb));
+    // }
 
-    return await new Promise(resolve => setTimeout(resolve, 2000));
+    // return await new Promise(resolve => setTimeout(resolve, 2000));
 }
 
 if (!isMainThread) {
