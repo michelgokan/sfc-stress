1e1e61f3d9fd8cb1d995ca8b0dbd8fd1852899df
Michel Gokan <michel.gokan@kau.se>
2020-10-08 19:18:46 +0200

    Rewriting memory intensive workload such that it fills the memory non-sequentially (Javid's suggestion)

--- workloads/mem-workload.js
+++ workloads/mem-workload.js
@@ -1,12 +1,22 @@
 const {Worker, isMainThread, parentPort, workerData} = require('worker_threads');
 const crypto = require("crypto");
 
-async function executeMemWorkload(dataSize) {
-    const onemb = require('./payload/onemb');
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
 
+async function executeMemWorkload(dataSize) {
     let bigStrArray = [];
     for (let i = 0; i < dataSize; i++) {
-        bigStrArray.push(crypto.randomBytes(i * 1024).toString('hex'));
+        bigStrArray.push(0)
+    }
+
+    for (let j = 0; j < 100; j++) {
+        for (let i = 0; i < dataSize; i++) {
+            let _index = getRandomInt(dataSize);
+            if (bigStrArray[_index] === 0)
+                bigStrArray[_index] = crypto.randomBytes(1024 * 1024).toString('hex');
+        }
     }
 
     return true; //await new Promise(resolve => setTimeout(resolve, 2000));
