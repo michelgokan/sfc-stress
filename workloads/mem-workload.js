e1aaada59a32c9ab7b6d93972542bb4107477916
Michel Gokan <michel.gokan@kau.se>
2020-10-08 19:35:59 +0200

    Fixing a bug and reverting the memory workload's function back to sequential mode

--- workloads/mem-workload.js
+++ workloads/mem-workload.js
@@ -8,19 +8,28 @@ function getRandomInt(max) {
 async function executeMemWorkload(dataSize) {
     let bigStrArray = [];
     for (let i = 0; i < dataSize; i++) {
-        bigStrArray.push(0)
-    }
-
-    for (let j = 0; j < 100; j++) {
-        for (let i = 0; i < dataSize; i++) {
-            let _index = getRandomInt(dataSize);
-            if (bigStrArray[_index] === 0)
-                bigStrArray[_index] = crypto.randomBytes(1024 * 1024).toString('hex');
-        }
+        bigStrArray.push(crypto.randomBytes(1024 * 1024).toString('hex'));
     }
 
     return true; //await new Promise(resolve => setTimeout(resolve, 2000));
 }
+
+// async function executeMemWorkload(dataSize) {
+//     let bigStrArray = [];
+//     for (let i = 0; i < dataSize; i++) {
+//         bigStrArray.push(0)
+//     }
+//
+//     for (let j = 0; j < 100; j++) {
+//         for (let i = 0; i < dataSize; i++) {
+//             let _index = getRandomInt(dataSize);
+//             if (bigStrArray[_index] === 0)
+//                 bigStrArray[_index] = crypto.randomBytes(1024 * 1024).toString('hex');
+//         }
+//     }
+//
+//     return true; //await new Promise(resolve => setTimeout(resolve, 2000));
+// }
 
 if (!isMainThread) {
     let result = executeMemWorkload(workerData.paramValue);
