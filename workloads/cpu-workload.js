7f090d5f796a78d331deb38413e8c0d159d4f152
Michel Gokan Khan <michel.gokan@spotin.com>
2023-03-08 08:24:20 +0100

    Trying some changes

--- workloads/cpu-workload.js
+++ workloads/cpu-workload.js
@@ -12,9 +12,13 @@ function executeCPUWorkload(workloadSize) {
     console.log("Workload size: " + workloadSize);
     for (let $i = 0; $i < workloadSize; $i++) {
         const prime_length = 100;
+        console.log("Before calling createDiffieHellman with prime_length= " + prime_length);
         const diffHell = crypto.createDiffieHellman(prime_length);
+        console.log("Before calling generateKeys - diffHell= " + diffHell);
         const key = diffHell.generateKeys('base64');
-        checksum(key);
+        console.log("Before calling checksum - key= " + key);
+        const chksum = checksum(key);
+        console.log("After calling checksum - checksum= " + chksum);
     }
 
     return true;
